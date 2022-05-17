954c9b7d1c4ff74ac91e382106a753bd9e088eb8
Danil <danilbe168@gmail.com>
2022-05-17 23:07:01 +0300

    question start

---
+++ js/question.js
@@ -0,0 +1,14 @@
+const questionBtn = document.querySelectorAll(".question_mark ");
+const answer = document.querySelectorAll(".question_answer");
+
+questionBtn.forEach((btn, index)=>{
+    btn.addEventListener("click", ()=>{
+        if(btn.classList.contains("question_mark_active")){
+            btn.classList.remove("question_mark_active");
+            answer[index].style.height = "0";
+        } else{
+            btn.classList.add("question_mark_active");
+            answer[index].style.height = "unset";
+        }
+    })
+})
